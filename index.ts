1295ed16203080b9f329e74127470ea33def3d76
Decagon <uchecharles223@gmail.com>
2023-02-20 02:42:46 +0100

    project setup

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+const app : Express = express();
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
